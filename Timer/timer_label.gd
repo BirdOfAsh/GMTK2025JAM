a85826aab591a3fa9fe8743f0ac34b2eda0cc08f
Phoenix Jordan <136138123+BirdOfAsh@users.noreply.github.com>
2025-07-30 18:10:44 -0500

    Initial Commit

---
+++ Timer/timer_label.gd
@@ -0,0 +1,18 @@
+extends Label3D
+
+@export_range(1.0, 60.0, 1.0) var waitTime : float
+
+@onready var timer : Timer = $Timer
+
+
+func _ready() -> void:
+	timer.wait_time = waitTime
+	timer.start()
+
+
+func _process(_delta: float) -> void:
+	text = str(float(int(timer.time_left * 10)) / 10)
+
+
+func _on_timer_timeout() -> void:
+	print("Timer ended call function")
