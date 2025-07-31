49520e1ee44a45afecfe45263ebde56c3ce1608c
Phoenix Jordan <136138123+BirdOfAsh@users.noreply.github.com>
2025-07-30 21:26:35 -0500

    Fixed jittery camera movement

--- Player/player.gd
+++ Player/player.gd
@@ -1,13 +1,15 @@
 extends CharacterBody3D
 
 @onready var camera : Camera3D = $Camera3D
+@onready var marker3d: Marker3D = $Marker3D
 
 
 var speed : int = 300
 var direction : Vector3
 var inputDirection : Vector2i
-var mouseSensitivity = 3
+var mouseSensitivity : int = 3
 
+var cameraRotation : Vector2
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -32,15 +34,26 @@ func _physics_process(delta: float) -> void:
 	if inputDirection != Vector2i.ZERO:
 		direction = transform.basis * Vector3(inputDirection.x, 0 , inputDirection.y).normalized()
 	
+	
+	rotateCamera()
+	
 	velocity = direction * speed * delta
 	move_and_slide()
 
 
-
 func _input(event: InputEvent) -> void:
 	
-	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		rotate_y(event.relative.x * -0.005 * mouseSensitivity)
-		camera.rotate_x(event.relative.y * -0.005 * mouseSensitivity)
+	if event is InputEventMouseMotion:
 		
-		camera.rotation_degrees.x = clampf(camera.rotation_degrees.x, -90, 90)
+		cameraRotation.y = event.relative.x * -0.005 * mouseSensitivity
+		cameraRotation.x = event.relative.y * -0.005 * mouseSensitivity
+
+func rotateCamera() -> void:
+	rotate_y(cameraRotation.y)
+	marker3d.rotate_x(cameraRotation.x)
+	
+	marker3d.rotation_degrees.x = clampf(marker3d.rotation_degrees.x, -90, 90)
+	
+	camera.global_transform = marker3d.get_global_transform_interpolated()
+	
+	cameraRotation = cameraRotation.lerp(Vector2.ZERO, 1)
