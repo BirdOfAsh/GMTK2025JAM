939664c5d0b5b97179f3dc0d96a4bb201ee4a6a7
Parker Vetter <parkervetter@gmail.com>
2025-07-31 21:57:13 -0500

    added spark particles

--- InteractableObjects/Scripts/resistor_holder.gd
+++ InteractableObjects/Scripts/resistor_holder.gd
@@ -1,14 +1,15 @@
 extends Node3D
 
 @onready var resistor_marker: Marker3D = $ResistorMarker
-
-
+@onready var particle: GPUParticles3D = $GPUParticles3D2
+@onready var particle1: GPUParticles3D = $GPUParticles3D
 var heldResistor : Resistor
 
 func interaction(player : CharacterBody3D):
 	if player.call("getHeldObject") is Resistor:
 		heldResistor = player.call("getHeldObject")
 		player.call("place", self)
-		
+		particle.set_emitting(true)
+		particle1.set_emitting(true)
 		heldResistor.disable()
 		heldResistor.setPosition(resistor_marker.position)
